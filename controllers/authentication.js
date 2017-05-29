f907165a52747c1e27fc97da5d25b8807d9e4cea
Scott Wilkins <scottwilkins@Scotts-mac.local>
2017-05-29 10:26:06 -0600

    add auth

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,4 +1,17 @@
-const User = require('../models/user')
+const jwt = require('jwt-simple');
+const User = require('../models/user');
+const config  = require('../config');
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret)
+}
+
+exports.signin = function(req, res, next) {
+  // user has already had email and password auth'd
+  // we just need to give them a token
+  res.send({ token: tokenForUser(req.user) });
+}
 
 exports.signup = function(req, res, next) {
   const email = req.body.email;
@@ -24,7 +37,7 @@ User.findOne({ email: email }, function(err, existingUser) {
   user.save(function(err) {
     if (err) { return next(err) }
 
-    res.json({ success: true })
+    res.json({ token: tokenForUser(user) })
   });
   // respond indicating user was created
 });
